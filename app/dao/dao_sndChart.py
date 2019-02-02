28319cc5242f8add18fd6e77029c14c6ea7f85a6
rubenchu <jazztronomers@gmail.com>
2019-02-02 16:23:21 +0900

    -

--- app/dao/dao_sndChart.py
+++ app/dao/dao_sndChart.py
@@ -1,15 +1,31 @@
 # import pyodbc
 import mysql.connector as mc
 import constant as cs
+import requests
+from datetime import datetime as dt
+from xml.etree import ElementTree as et
 
 class Database:
 
-    def list_employees(self, code):
-        ip = cs.ip
-        id = cs.id
-        pw = cs.pw
-        dbScheme = cs.dbScheme
-        self.cnxn = mc.connect(host=ip, database=dbScheme, user=id, password=pw)
+
+    ip = cs.ip
+    id = cs.id
+    pw = cs.pw
+    dbScheme = cs.dbScheme
+    cnxn = ''
+
+    def getConn(self):
+
+        self.cnxn = mc.connect(host=self.ip, database=self.dbScheme, user=self.id, password=self.pw)
+
+    def closeConn(self):
+
+        self.cnxn.close()
+
+
+    def sndChart(self, code):
+
+        self.getConn()
         cursor = self.cnxn.cursor()
         query = '''
                            SELECT A.STOCKNAME, A.STOCKCODE, CAST(A.DATE AS char) AS DATE, B.ADJRATIO 
@@ -55,141 +71,184 @@ class Database:
                   [dict(zip([column[0] for column in cursor.description], row))
                    for row in cursor.fetchall()]}
 
-        self.cnxn.close()
+
+
+        self.closeConn()
         return rt
 
-
-def employees(code=None):
-    def db_query():
-        db = Database()
-        emps = db.list_employees(code)
-        return emps
-
-    res = db_query()
-    return res
+    def sndRank(self, column, interval, order, by):
 
 
-def sndRank(column, interval, order, by):
-
-
-    queryhead = '''
+        queryhead = '''
+        
+                SELECT B.STOCKNAME, DATE, CLOSE, 
     
-            SELECT B.STOCKNAME, DATE, CLOSE, 
-
-
-    '''
-
-    toselect = ['P','I','F']
-
-    for each in column:
-        toselect.append(each)
-
-
-    querycont = ''
-    for eachcolumn in toselect:
-        for eachinterval in interval:
-            querycont = querycont + str(eachcolumn)+str(eachinterval)+', '
-
-    print('[DEBUG]' , querycont[:-2])
-
-    querytail = '''
     
-        FROM jazzdb.T_STOCK_SND_ANALYSIS_RESULT_TEMP A
-        JOIN jazzdb.T_STOCK_CODE_MGMT B USING (STOCKCODE)
-        JOIN jazzdb.T_DATE_INDEXED C USING (DATE)
-        WHERE 1=1
-        AND (I1>0 OR F1>0) 
-        AND C.CNT = 0
-        ORDER BY %s1 %s
-        LIMIT 100
+        '''
+
+        toselect = ['P','I','F']
+
+        for each in column:
+            toselect.append(each)
+
+
+        querycont = ''
+        for eachcolumn in toselect:
+            for eachinterval in interval:
+                querycont = querycont + str(eachcolumn)+str(eachinterval)+', '
+
+        print('[DEBUG]' , querycont[:-2])
+
+        querytail = '''
+        
+            FROM jazzdb.T_STOCK_SND_ANALYSIS_RESULT_TEMP A
+            JOIN jazzdb.T_STOCK_CODE_MGMT B USING (STOCKCODE)
+            JOIN jazzdb.T_DATE_INDEXED C USING (DATE)
+            WHERE 1=1
+            AND (I1>0 OR F1>0) 
+            AND C.CNT = 0
+            ORDER BY %s1 %s
+            LIMIT 100
+        
+        '''%(order[0],by[0])
+
+        fullquery = queryhead + querycont[:-2] + querytail
+        print('fq: \n ', fullquery)
+
+        self.getConn()
+        cursor = self.cnxn.cursor()
+        cursor.execute(fullquery)
+
+
+
+        column = cursor.column_names
+        rt = {'result':
+                  [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]}
+
+        dt =rt['result'][0]['DATE']
+        # dt = '2019-12-12'
+
+        self.closeConn()
+
+        return column, rt, dt
+
+
+    def sndIndependent(slef,order, by):
+
+        column = ['FR', 'IR', 'YR', 'SR', 'TR', 'FNR', 'ISR', 'NTR', 'BKR', 'OCR']
+        interval = [1,5]
+        queryhead = '''
     
-    '''%(order[0],by[0])
+                SELECT B.STOCKNAME, DATE, CLOSE, 
+    
+    
+        '''
 
-    fullquery = queryhead + querycont[:-2] + querytail
-    print('fq: \n ', fullquery)
-
-
-
-    ip = cs.ip
-    id = cs.id
-    pw = cs.pw
-    dbScheme = cs.dbScheme
-    cnxn = mc.connect(host=ip, database=dbScheme, user=id, password=pw)
-    cursor = cnxn.cursor()
-    cursor.execute(fullquery)
+        toselect = ['P', 'F', 'I']
 
 
 
-    column = cursor.column_names
-    rt = {'result':
-              [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]}
+        querycont = ''
+        for eachcolumn in toselect:
+            for eachinterval in interval:
+                querycont = querycont + str(eachcolumn) + str(eachinterval) + ', '
+
+        for each in column:
+            querycont = querycont + str(each) + ', '
+
+        print('[DEBUG]', querycont[:-2])
+
+        querytail = '''
+    
+            FROM jazzdb.T_STOCK_SND_ANALYSIS_RESULT_TEMP A
+            JOIN jazzdb.T_STOCK_CODE_MGMT B USING (STOCKCODE)
+            JOIN jazzdb.T_DATE_INDEXED C USING (DATE)
+            WHERE 1=1
+            AND (I1>0 OR F1>0) 
+            AND C.CNT = 0
+            ORDER BY %sR %s
+            LIMIT 100
+    
+        ''' % (order[0], by[0])
+
+        fullquery = queryhead + querycont[:-2] + querytail
+        print('fq: \n ', fullquery)
+
+        ip = cs.ip
+        id = cs.id
+        pw = cs.pw
+        dbScheme = cs.dbScheme
+        cnxn = mc.connect(host=ip, database=dbScheme, user=id, password=pw)
+        cursor = cnxn.cursor()
+        cursor.execute(fullquery)
+
+        column = cursor.column_names
+        rt = {'result':
+                  [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]}
+
+        dt = rt['result'][0]['DATE']
+        # dt = '2019-12-12'
+
+        cnxn.close()
+
+        return column, rt, dt
 
 
-
-    dt =rt['result'][0]['DATE']
-    # dt = '2019-12-12'
-
-    cnxn.close()
-
-    return column, rt, dt
+    def stockinfo(self,stockcode):
 
 
-def sndIndependent(order, by):
+        print(stockcode)
+        self.getConn()
+        cursor = self.cnxn.cursor()
+        query = '''
 
-    column = ['FR', 'IR', 'YR', 'SR', 'TR', 'FNR', 'ISR', 'NTR', 'BKR', 'OCR']
-    interval = [1,5]
-    queryhead = '''
+                               SELECT A.STOCKCODE
+                               FROM jazzdb.T_STOCK_CODE_MGMT A
+                               WHERE 1=1
+                               AND (STOCKCODE = '%s' OR STOCKNAME = '%s')
 
-            SELECT B.STOCKNAME, DATE, CLOSE, 
+                            ;
+        ''' % (stockcode,stockcode)
+        cursor.execute(query)
+
+        code =  cursor.fetchall()[0][0]
+        print(code)
+        self.closeConn()
+        url = 'http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=%s' %(code)
+        xml = et.fromstring(requests.get(url).content.strip())
+        # keys = ['day_Date','day_Start','day_High','day_Low','day_EndPrice','day_Volume','day_getAmount']
+        keys = ['day_Date', 'day_Start', 'day_High', 'day_Low', 'day_EndPrice', 'day_Volume']
+        convertKeys = {
+
+            'day_Date':'DATE',
+            'day_Start':'OPEN',
+            'day_High':'HIGH',
+            'day_Low':'LOW',
+            'day_EndPrice':'CLOSE',
+            'day_Volume':'VOLUME',
+        }
+        rtlist = []
+        for each in xml:
+            for eachnode in each:
+                eachrow = {}
+                if(eachnode.tag == 'DailyStock'):
+                    for eachkey in keys:
+                        # 날짜 포맷팅
+                        if(eachkey=='day_Date'):
+                            #eachrow.append('20'+eachnode.attrib[eachkey].replace('/','-'))
+                            eachrow[convertKeys[eachkey]]='20'+eachnode.attrib[eachkey].replace('/','-')
+                        else:
+
+                            eachrow[convertKeys[eachkey]] = int(eachnode.attrib[eachkey].replace(',',''))
+
+                    rtlist.append(eachrow)
 
 
-    '''
+        if(len(rtlist)>0):
+            return rtlist[0]
+        else:
+            return None
 
-    toselect = ['P', 'F', 'I']
+    if __name__ == '__main__':
 
-
-
-    querycont = ''
-    for eachcolumn in toselect:
-        for eachinterval in interval:
-            querycont = querycont + str(eachcolumn) + str(eachinterval) + ', '
-
-    for each in column:
-        querycont = querycont + str(each) + ', '
-
-    print('[DEBUG]', querycont[:-2])
-
-    querytail = '''
-
-        FROM jazzdb.T_STOCK_SND_ANALYSIS_RESULT_TEMP A
-        JOIN jazzdb.T_STOCK_CODE_MGMT B USING (STOCKCODE)
-        JOIN jazzdb.T_DATE_INDEXED C USING (DATE)
-        WHERE 1=1
-        AND (I1>0 OR F1>0) 
-        AND C.CNT = 0
-        ORDER BY %sR %s
-        LIMIT 100
-
-    ''' % (order[0], by[0])
-
-    fullquery = queryhead + querycont[:-2] + querytail
-    print('fq: \n ', fullquery)
-
-    ip = cs.ip
-    id = cs.id
-    pw = cs.pw
-    dbScheme = cs.dbScheme
-    cnxn = mc.connect(host=ip, database=dbScheme, user=id, password=pw)
-    cursor = cnxn.cursor()
-    cursor.execute(fullquery)
-
-    column = cursor.column_names
-    rt = {'result':
-              [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]}
-
-    dt = rt['result'][0]['DATE']
-    # dt = '2019-12-12'
-
-    cnxn.close()
-
-    return column, rt, dt
+        stockinfo('079940')
