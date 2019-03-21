184afec3d887299c6f84ea04af77084119d9cb5a
rubenchu <jazztronomers@gmail.com>
2019-03-21 23:25:15 +0900

    -

--- crawl/11.crawlRunner_server.py
+++ crawl/11.crawlRunner_server.py
@@ -26,6 +26,16 @@ def processrunner_forwin(srcname,itrnum, wincode):
         print("[INFO] ", srcname, itrnum, ' PROCESS END, TIME SPEND :', dt.now() - processStart)
 
 
+def processrunner_forwin_prev(srcname,itrnum, wincode, date):
+
+    processStart = dt.now()
+    print('[INFO] RUNNING SCRIPT : ', srcname , ' for ', itrnum ,'times')
+
+    for itr in range(itrnum):
+
+        process = subprocess.Popen(["python", srcname, wincode,date])
+        print(process.communicate()[0])  # 프로세스가 끝날때까지 기다리는 명령
+        print("[INFO] ", srcname, itrnum, ' PROCESS END, TIME SPEND :', dt.now() - processStart)
 
 
 wholeStart = dt.now()
@@ -39,26 +49,32 @@ if(len(sys.argv)==0):
 else :
 
 
-    #
+
     # processrunner('01.boot.py',1)
     #
-    # for winCode in ['36', '42', '44', '45', '54']:
-    #         processrunner_forwin('07.crawl_forwin.py',3,winCode)
-    #
-    # processrunner('99.crawl_ohlc.py', 3)
     # processrunner('03.crawl_snd_basic.py',3)
     # processrunner('04.dateindexupdate.py',1)
     # processrunner('05.sndBasicAnalysis.py',1)
-
     #
-    # for winCode in ['36', '42', '44', '41']:
-    #         processrunner_forwin('07.crawl_forwin.py',3,winCode)
-    # #
-    for winCode in ['61', '58', '43', '35', '41']:
-            processrunner_forwin('07.crawl_forwin.py',3,winCode)
+    # for winCode in ['61', '58', '43', '35', '41']:
+    #         processrunner_forwin('07.crawl_forwin.py',1,winCode)
+    #
+    #
+    # for winCode in ['36','42', '44', '45', '54']:
+    #         processrunner_forwin('07.crawl_forwin.py',1,winCode)
+    #
+    # for winCode in ['33', '06', '03', '37']:
+    #     processrunner_forwin('07.crawl_forwin.py',1,winCode)
+    #
+    # processrunner('08.merge_forwin.py',1)
 
-    processrunner('08.merge_forwin.py',1)
 
 
+     for winCode in ['33', '06', '03', '37']:
+            # processrunner_forwin_prev('07.crawl_forwin.py', 3, winCode, '20190228')
+        processrunner_forwin_prev('07.crawl_forwin.py', 3, winCode, '20190321')
+
+
+     processrunner('08.merge_forwin.py',1)
 
 print("[INFO]  WHOLE PROCESS END, TIME SPEND :'", dt.now() - wholeStart)
